cf2d792449d356ca5db9ef933bdc63369946d3c8
teevik <teemu.vikoren@gmail.com>
2024-01-30 11:05:28 +0100

    Initial

---
+++ code/config.php
@@ -0,0 +1,12 @@
+<?php
+$dbhost = "192.168.128.111";
+$dbport = "26257";
+$db = "bf";
+$dbuser = "bfuser";
+$dbpassw = '';
+$webhost = '10.212.168.112';
+$weburl = 'http://' . $webhost ;
+$frontpage_limit = "1500";
+$use_activity_cutoff_days = 14;
+$use_fast_random_search = 1;
+?>
